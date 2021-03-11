d323eacdea595e84ccba77549d99413bf955b807
Erik Rasmus Nilsson <info@rasmusnilsson.se>
2021-03-11 15:56:54 +0100

    Made the logotype clickable, navigates back to index when clicked

--- src/components/logotype/index.js
+++ src/components/logotype/index.js
@@ -1,7 +1,12 @@
 import styles from './index.module.css';
+import { useHistory } from 'react-router-dom';
 
-const logotype = () => {
-    return <img alt='logotype' className={ styles.Logotype } src={`${process.env.PUBLIC_URL}/images/logotype.png`} />
+const Logotype = () => {
+    const history = useHistory();
+
+    const onClick = () => history.push("/")
+
+    return <img onClick={ onClick } alt='logotype' className={ styles.Logotype } src={`${process.env.PUBLIC_URL}/images/logotype.png`} />
 };
 
-export { logotype as Logotype };
+export { Logotype };
